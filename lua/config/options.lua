9b1c548c24bfa4ee651878f0572b657bf78f4970
FauzanAcyuto <uzanbaroto@gmail.com>
2025-12-24 20:57:48 +0700

    nvim updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  -- ... other options
+  plugins = {
+    spec = {
+      {
+        "folke/snacks.nvim",
+        opts = {
+          image = {
+            enabled = true, -- This is the crucial part for image previews!
+          },
+          -- Other snacks options like picker, terminal, etc.
+        },
+      },
+    },
+  },
+}
+--
