4743a7f726bc44ad12758e3b5fe0c96c950479c6
FauzanAcyuto <uzanbaroto@gmail.com>
2026-01-25 13:14:05 +0000

    modified:   lazy-lock.json 	modified:   lua/config/options.lua 	lua/plugins/obsidian-nvim.lua
    	modified:   lazy-lock.json
    	modified:   lua/config/options.lua
    	lua/plugins/obsidian-nvim.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+<<<<<<< HEAD
 return {
   -- ... other options
   plugins = {
@@ -18,3 +19,14 @@ return {
   },
 }
 --
+=======
+-- In ~/.config/nvim/lua/config/options.lua
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+}
+vim.opt.clipboard = "unnamedplus" -- Also set clipboard to use unnamedplus
+>>>>>>> 5c69c62 (	modified:   lazy-lock.json)
